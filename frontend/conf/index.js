d85391e7d568e85097855d9ebec3e4f940ebd551
CrioUser <crio-user@criodo.com>
2023-03-04 21:05:48 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURES

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://3.110.249.171:8082" };
+const config = { backendEndpoint: "http://13.232.249.219:8082" };
 
 export default config;
--- frontend/modules/adventures_page.js
+++ frontend/modules/adventures_page.js
@@ -5,14 +5,21 @@ import config from "../conf/index.js";
 function getCityFromURL(search) {
   // TODO: MODULE_ADVENTURES
   // 1. Extract the city id from the URL's Query Param and return it
-
+  let cityId = new URLSearchParams(search);
+  return cityId.get("city");
 }
 
 //Implementation of fetch call with a paramterized input based on city
 async function fetchAdventures(city) {
   // TODO: MODULE_ADVENTURES
   // 1. Fetch adventures using the Backend API and return the data
-
+  try {
+    let res = await fetch(config.backendEndpoint + `/adventures?city=${city}`)
+    let data = await res.json();
+    return data;
+  } catch {
+    return null;
+  }
 }
 
 //Implementation of DOM manipulation to add adventures for the given city from list of adventures
@@ -20,74 +27,110 @@ function addAdventureToDOM(adventures) {
   // TODO: MODULE_ADVENTURES
   // 1. Populate the Adventure Cards and insert those details into the DOM
 
+  // Getting connected with out html div as a div parent by id
+  let rowDiv = document.getElementById("data");
+  adventures.forEach((key) => {
+    // creating new div for out fetched data.
+    let dataDiv = document.createElement("div");
+    // creating bootstrap grid class for our data 
+    dataDiv.className = "col-6 col-lg-3 mb-4";
+    // creating elements using innerHTML for our Card
+    dataDiv.innerHTML = `
+    <a href = "detail/?adventure=${key.id}" id=${key.id}>
+
+    
+      <div class = "activity-card">
+          
+         <div class = "category-banner"> ${key.category}</div>
+
+  
+         <img class = "activity-card img" class ="image-responsive" src = "${key.image}"/>
+
+            <div class = "d-flex justify-content-between w-100 p-2">
+           <h5 style = "text-left"> ${key.name} </h5>
+            <p> ${key.costPerHead} </p>
+           </div> 
+
+  
+          <div class = "d-flex justify-content-between w-100 p-2">
+            <h5 class = "text-left"> Duration </h5>
+            <p> ${key.duration} </p>
+           </div>  
+         </div>
+         </a>`;
+    rowDiv.appendChild(dataDiv);
+  });
 }
 
-//Implementation of filtering by duration which takes in a list of adventures, the lower bound and upper bound of duration and returns a filtered list of adventures.
-function filterByDuration(list, low, high) {
-  // TODO: MODULE_FILTERS
-  // 1. Filter adventures based on Duration and return filtered list
-
-}
-
-//Implementation of filtering by category which takes in a list of adventures, list of categories to be filtered upon and returns a filtered list of adventures.
-function filterByCategory(list, categoryList) {
-  // TODO: MODULE_FILTERS
-  // 1. Filter adventures based on their Category and return filtered list
-
-}
-
-// filters object looks like this filters = { duration: "", category: [] };
-
-//Implementation of combined filter function that covers the following cases :
-// 1. Filter by duration only
-// 2. Filter by category only
-// 3. Filter by duration and category together
-
-function filterFunction(list, filters) {
-  // TODO: MODULE_FILTERS
-  // 1. Handle the 3 cases detailed in the comments above and return the filtered list of adventures
-  // 2. Depending on which filters are needed, invoke the filterByDuration() and/or filterByCategory() methods
 
 
-  // Place holder for functionality to work in the Stubs
-  return list;
-}
 
-//Implementation of localStorage API to save filters to local storage. This should get called everytime an onChange() happens in either of filter dropdowns
-function saveFiltersToLocalStorage(filters) {
-  // TODO: MODULE_FILTERS
-  // 1. Store the filters as a String to localStorage
+  //Implementation of filtering by duration which takes in a list of adventures, the lower bound and upper bound of duration and returns a filtered list of adventures.
+  function filterByDuration(list, low, high) {
+    // TODO: MODULE_FILTERS
+    // 1. Filter adventures based on Duration and return filtered list
 
-  return true;
-}
+  }
 
-//Implementation of localStorage API to get filters from local storage. This should get called whenever the DOM is loaded.
-function getFiltersFromLocalStorage() {
-  // TODO: MODULE_FILTERS
-  // 1. Get the filters from localStorage and return String read as an object
+  //Implementation of filtering by category which takes in a list of adventures, list of categories to be filtered upon and returns a filtered list of adventures.
+  function filterByCategory(list, categoryList) {
+    // TODO: MODULE_FILTERS
+    // 1. Filter adventures based on their Category and return filtered list
+
+  }
+
+  // filters object looks like this filters = { duration: "", category: [] };
+
+  //Implementation of combined filter function that covers the following cases :
+  // 1. Filter by duration only
+  // 2. Filter by category only
+  // 3. Filter by duration and category together
+
+  function filterFunction(list, filters) {
+    // TODO: MODULE_FILTERS
+    // 1. Handle the 3 cases detailed in the comments above and return the filtered list of adventures
+    // 2. Depending on which filters are needed, invoke the filterByDuration() and/or filterByCategory() methods
 
 
-  // Place holder for functionality to work in the Stubs
-  return null;
-}
+    // Place holder for functionality to work in the Stubs
+    return list;
+  }
 
-//Implementation of DOM manipulation to add the following filters to DOM :
-// 1. Update duration filter with correct value
-// 2. Update the category pills on the DOM
+  //Implementation of localStorage API to save filters to local storage. This should get called everytime an onChange() happens in either of filter dropdowns
+  function saveFiltersToLocalStorage(filters) {
+    // TODO: MODULE_FILTERS
+    // 1. Store the filters as a String to localStorage
 
-function generateFilterPillsAndUpdateDOM(filters) {
-  // TODO: MODULE_FILTERS
-  // 1. Use the filters given as input, update the Duration Filter value and Generate Category Pills
+    return true;
+  }
 
-}
-export {
-  getCityFromURL,
-  fetchAdventures,
-  addAdventureToDOM,
-  filterByDuration,
-  filterByCategory,
-  filterFunction,
-  saveFiltersToLocalStorage,
-  getFiltersFromLocalStorage,
-  generateFilterPillsAndUpdateDOM,
-};
+  //Implementation of localStorage API to get filters from local storage. This should get called whenever the DOM is loaded.
+  function getFiltersFromLocalStorage() {
+    // TODO: MODULE_FILTERS
+    // 1. Get the filters from localStorage and return String read as an object
+
+
+    // Place holder for functionality to work in the Stubs
+    return null;
+  }
+
+  //Implementation of DOM manipulation to add the following filters to DOM :
+  // 1. Update duration filter with correct value
+  // 2. Update the category pills on the DOM
+
+  function generateFilterPillsAndUpdateDOM(filters) {
+    // TODO: MODULE_FILTERS
+    // 1. Use the filters given as input, update the Duration Filter value and Generate Category Pills
+
+  }
+  export {
+    getCityFromURL,
+    fetchAdventures,
+    addAdventureToDOM,
+    filterByDuration,
+    filterByCategory,
+    filterFunction,
+    saveFiltersToLocalStorage,
+    getFiltersFromLocalStorage,
+    generateFilterPillsAndUpdateDOM,
+  };
